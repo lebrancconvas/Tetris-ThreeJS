d43fe6fb1f9cc787e48f1f575670e5fc20bdb4fa
lebrancconvas <lebrancconvas@gmail.com>
2022-03-22 16:16:38 +0700

    🎨 Apply Factory Pattern.

--- tetris/src/GameObject/VisualObject/Tetrominoes/TetrominoS.ts
+++ tetris/src/GameObject/VisualObject/Tetrominoes/TetrominoS.ts
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import ObjectFactory from '../ObjectFactory';
 
 // Create Mesh and then add mesh to Scene. 
 
@@ -12,33 +13,17 @@ class TetrominoS extends THREE.Mesh {
 	}
 
 	create() {
-		const cube1 = new THREE.Mesh(
-			new THREE.BoxGeometry(),
-			new THREE.MeshBasicMaterial({color: this.Color})
-		);
-		cube1.position.x = 3;
-		cube1.position.y = 0.5;
-	
-		const cube2 = new THREE.Mesh(
-			new THREE.BoxGeometry(),
-			new THREE.MeshBasicMaterial({color: this.Color})
-		);
-		cube2.position.x = 4;
-		cube2.position.y = 0.5;
-	
-		const cube3 = new THREE.Mesh(
-			new THREE.BoxGeometry(),
-			new THREE.MeshBasicMaterial({color: this.Color})
-		);
-		cube3.position.x = 4;
-		cube3.position.y = 1.5;
-	
-		const cube4 = new THREE.Mesh(
-			new THREE.BoxGeometry(),
-			new THREE.MeshBasicMaterial({color: this.Color})
-		);
-		cube4.position.x = 5;
-		cube4.position.y = 1.5;
+		const cube1 = new ObjectFactory(this.scene);
+		cube1.createCube(this.Color, 3, 0.5, 0);
+
+		const cube2 = new ObjectFactory(this.scene);
+		cube1.createCube(this.Color, 4, 0.5, 0);
+
+		const cube3 = new ObjectFactory(this.scene);
+		cube1.createCube(this.Color, 4, 1.5, 0);
+		
+		const cube4 = new ObjectFactory(this.scene);
+		cube1.createCube(this.Color, 5, 1.5, 0);
 	
 		this.scene.add(cube1, cube2, cube3, cube4);
 	}
